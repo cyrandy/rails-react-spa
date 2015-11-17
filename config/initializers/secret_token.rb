03af3ed76cffe2875b4282726df1323a6f822098
cyrandy <cyrandy@gmail.com>
2015-11-17 15:42:26 +0800

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsReactSpa::Application.config.secret_token = '41124b585747cb1631cf1869c1234879d04221c760cd1107e4f62bed78f4cbd8e1094193edadc2ba80e63daedf0c4f0b8f235b247ee3dfdced96e3c9ce170bb0'
